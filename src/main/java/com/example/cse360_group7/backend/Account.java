07d697f7f219c5c9f7c706ff65a35136243df290
ifavila <ifavila8@gmail.com>
2022-03-17 13:26:26 -0700

    Added backend files
    includes: account(abstract for employee and customer), coupon, employee, and menu item

---
+++ src/main/java/com/example/cse360_group7/backend/Account.java
@@ -0,0 +1,13 @@
+
+public abstract class Account {
+	protected String username;
+	protected String password;
+	
+	//getters
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
---
+++ src/main/java/com/example/cse360_group7/backend/Employee.java
@@ -0,0 +1,8 @@
+
+public class Employee extends Account{
+	//constructor
+	public Employee(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+}
