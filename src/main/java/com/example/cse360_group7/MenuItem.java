4bf61254527e77c60aabbe200ac6faffb05deea5
ifavila <ifavila8@gmail.com>
2022-03-17 13:13:35 -0700

    Added employee, menuitem, coupon, account class

---
+++ src/main/java/com/example/cse360_group7/MenuItem.java
@@ -0,0 +1,36 @@
+
+public class MenuItem {
+	protected String itemName;
+	protected int calories;
+	protected Double price;
+	//TODO: Something image related?
+	
+	//constructors - no default
+	public MenuItem(String itemName, int calories, Double price) {
+		this.itemName = itemName;
+		this.calories = calories;
+		this.price = price;
+	}
+	
+	//getters
+	public String getItemName() {
+		return itemName;
+	}
+	public int getCalories() {
+		return calories;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	
+	//setters - editing menu items on employee side(no customer interaction)
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
